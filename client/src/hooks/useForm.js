ccddb5071215b4fe9891bb2305bfe3892cf1eeeb
Avery Burns <a.aburns95@gmail.com>
2020-11-08 22:40:44 -0600

    useLocalStorage hook created, useForm in prog, test created

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,32 @@
+import { useLocalStorage } from "./useLocalStorage";
+
 // write your custom hook here to control your checkout form
+
+// replacement for following logic needed in this hook
+
+// const CheckoutForm = (props) => {
+// const CheckoutForm = (props) => {
+//   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+//   const [values, setValues] = useState(initialValue);
+
+//   const handleChanges = (e) => {
+//     setValues({ ...values, [e.target.name]: e.target.value });
+//   };
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setShowSuccessMessage(true);
+//   };
+
+export const useForm = (key, initialValues, callback) => {
+    const [values, setValues] = useLocalStorage(key, initialValues);
+
+    const handleChanges = (e) => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const
+}
